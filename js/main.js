713061560c7ab74416d067217559bf4b71bc6cc7
jdarkes <josephdarkes@gmail.com>
2015-09-03 00:09:35 -0700

    Practice with forms and Javascript to create a mood teller

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+function showMood() {
+	name = document.getElementById("name").value;
+	mood = document.getElementById("mood").value;
 
+	if(!name || !mood){
+		alert("Please fill in all fields.");
+		return false;
+	}
+
+	if(mood == "happy"){
+			face =" :) "
+	}	else if(mood == "sad"){
+			face =" :( "
+	}	else {
+			face =" :| "
+	}
+
+	moodString = name + " is " + mood + " today" + face;
+	holder = document.getElementById("output");
+	holder.innerHTML = moodString;
+}
+
+function clearMood() {
+	document.getElementById("moodForm").reset();
+	holder = document.getElementById("output");
+	holder.innerHTML = "";
+}
